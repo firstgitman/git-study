534fecd4decd52afcc3c266bfc148d7c52f579fd
Cemen <firstman@i.ua>
2018-05-04 15:41:39 +0300

    add getMontchName

---
+++ js/getMonthName.js
@@ -0,0 +1,16 @@
+function getMonthName(place) {
+	const months = [
+		'january',
+		'february',
+		'march',
+		'april',
+		'may',
+		'june',
+		'jule',
+		'august',
+		'september',
+		'october',
+		'november',
+		'desember'
+	]
+}
